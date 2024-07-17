56faa380ff6fa8ea42b03489bb81a93c15b4b0db
y0tik <yotik_sharma@outlook.com>
2024-07-18 00:35:14 +0530

    added plan card and shortcuts

---
+++ app/components/plan-card.tsx
@@ -0,0 +1,19 @@
+import { Button } from "./ui/button";
+import { Card, CardTitle } from "./ui/card";
+
+export const PlanCard = () => {
+  return (
+    <Card className="px-6 py-6">
+      <CardTitle className="text-lg">Current Plan</CardTitle>
+      <div className="px-4 py-2 rounded-md text-primary-foreground bg-primary mt-3">
+        Premium
+      </div>
+      <div className="mt-3 text-muted-foreground">
+        Next Billing on 12/23/2024
+      </div>
+      <Button variant="link" className="mt-2 h-auto p-0">
+        More Details
+      </Button>
+    </Card>
+  );
+};
--- app/routes/_private+/overview/AdminOverview.tsx
+++ app/routes/_private+/overview/AdminOverview.tsx
@@ -1,3 +1,4 @@
+import { Link } from "@remix-run/react";
 import {
   FilesIcon,
   TicketIcon,
@@ -6,7 +7,10 @@ import {
   type LucideIcon,
 } from "lucide-react";
 import { useMemo } from "react";
+import { PlanCard } from "~/components/plan-card";
 import TicketListCard from "~/components/ticket-list";
+import { Button } from "~/components/ui/button";
+import { Card, CardTitle } from "~/components/ui/card";
 import Statistic from "~/components/ui/statistic";
 import { tickets } from "~/fakedata";
 
@@ -35,23 +39,48 @@ export default function AdminOverview() {
   );
   return (
     <div className="h-full flex flex-col">
-      <div className="grid grid-cols-4 gap-6">
+      <div className="flex space-x-6">
         {tempStats.map((s) => (
-          <Statistic
-            key={s.name}
-            title={s.name}
-            number={s.value}
-            prefix={""}
-            Icon={s.Icon}
-            description={""}
-          />
+          <div key={s.name} className="w-3/12">
+            <Statistic
+              title={s.name}
+              number={s.value}
+              prefix={""}
+              Icon={s.Icon}
+              description={""}
+            />
+          </div>
         ))}
       </div>
-      <div className="grow flex mt-6 min-h-0">
-        <div className="w-7/12">
-          <div>TODO : CARDS & QUICK LINKS</div>
+      <div className="grow space-x-6 flex mt-6 min-h-0">
+        <div className="w-3/12">
+          <PlanCard />
         </div>
-        <div className="w-5/12 flex flex-col">
+        <div className="w-3/12">
+          <Card>
+            <div className="p-6">
+              <CardTitle className="text-base text-primary">
+                Shortcuts
+              </CardTitle>
+              <div className="space-y-3 mt-4">
+                {[
+                  { name: "Create User", to: "/user/create" },
+                  { name: "Update Settings", to: "/settings" },
+                ].map((e) => (
+                  <Button
+                    className="w-full"
+                    variant="outline"
+                    key={e.name}
+                    asChild
+                  >
+                    <Link to={e.to}>{e.name}</Link>
+                  </Button>
+                ))}
+              </div>
+            </div>
+          </Card>
+        </div>
+        <div className="w-6/12 flex flex-col">
           <TicketListCard tickets={sortedTickets} />
         </div>
       </div>
