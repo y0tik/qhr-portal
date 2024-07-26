2adef88ade28bff495128a27b13d2ff530740a89
y0tik <yotik_sharma@outlook.com>
2024-07-26 23:40:22 +0530

    company overview card

--- app/routes/_alumni+/me.tsx
+++ app/routes/_alumni+/me.tsx
@@ -145,14 +145,57 @@ const LatestNewsCard = () => {
 
 const CompanyOverviewCard = () => {
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle>Company Overview</CardTitle>
-      </CardHeader>
-      <CardContent>
-        <div className="space-y-4 overflow-hidden">
-          {newsData.map((n) => (
-            <NewsCardItem key={n.title} {...n} />
+    <Card className="bg-transparent text-white border relative overflow-clip">
+      <div className="absolute inset-0 bg-red-400">
+        <img
+          className="h-full w-full object-cover"
+          src="https://images.unsplash.com/photo-1600880292089-90a7e086ee0c?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt=""
+        />
+      </div>
+      <div className="inset-0 absolute z-10 bg-gradient-to-t -mt-40 from-black to-transparent"></div>
+      <CardContent className="text-center pb-0 relative z-20">
+        <div className="mt-8 inline-block w-[250px] py-4 rounded-lg opacity-80 backdrop-blur-lg bg-white text-black">
+          <div className="flex justify-center items-center text-lg font-medium ">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="mr-2 h-16 w-16"
+            >
+              <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
+            </svg>
+            Acme Inc
+          </div>
+        </div>
+        <div className="mt-4 text-lg">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </div>
+        <div className="mt-1 text-sm text-white opacity-60">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam deserunt
+          omnis nulla quaerat iste similique commodi eligendi autem omnis nulla
+          quaerat iste similique commodi eligendi autem
+        </div>
+        <div className="grid grid-cols-4 pt-5 pb-6 -mx-5 px-10 bg-blue-800 text-white bg-opacity-60 mt-6">
+          {[
+            { stat: "73M+", desc: "Developers" },
+            { stat: "4M+", desc: "Organizations" },
+            { stat: "120+", desc: "Offices / Branches" },
+            { stat: "30+", desc: "Years in Industry" },
+          ].map((e) => (
+            <div
+              key={e.stat}
+              className="flex flex-col items-center justify-center"
+            >
+              <dt className="mb-1 text-3xl font-extrabold">{e.stat}</dt>
+              <dd className="text-white text-opacity-60 text-sm dark:text-gray-400">
+                {e.desc}
+              </dd>
+            </div>
           ))}
         </div>
       </CardContent>
@@ -163,9 +206,13 @@ const CompanyOverviewCard = () => {
 export default function AlumniOverviewPage() {
   return (
     <div className="flex-1 container mx-auto py-4 lg:px-0">
-      <div className="grid grid-cols-2 lg:grid-cols-2 gap-4 bg-white overflow-auto">
-        <CompanyOverviewCard />
-        <LatestNewsCard />
+      <div className="grid grid-cols-2 lg:grid-cols-6 gap-4 bg-white overflow-auto">
+        <div className="col-span-3">
+          <CompanyOverviewCard />
+        </div>
+        <div className="col-span-3">
+          <LatestNewsCard />
+        </div>
         <div className="flex h-full rounded-lg border bg-slate-200"></div>
         <div className="flex h-full rounded-lg border bg-slate-200"></div>
       </div>
